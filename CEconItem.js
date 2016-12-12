773e1f7b54b93dbb72c1038e4af3ef9a5ab88777
sebmorris <seb.morris@btinternet.com>
2016-12-12 17:15:09 +0000

    First

---
+++ CEconItem.js
@@ -0,0 +1,77 @@
+'use strict';
+
+module.exports = CEconItem;
+
+function CEconItem(item, descriptions, contextID) {
+	var thing;
+	for(thing in item) {
+		if(item.hasOwnProperty(thing)) {
+			this[thing] = item[thing];
+		}
+	}
+
+	this.assetid = this.id = (this.id || this.assetid);
+	this.instanceid = this.instanceid || '0';
+	this.amount = parseInt(this.amount, 10);
+	this.contextid = this.contextid || contextID.toString();
+
+	// Merge the description
+	if(descriptions) {
+		let description;
+		for (let i = 0; i < descriptions.length; i++) {
+			if (
+				descriptions[i].classid === this.classid &&
+				descriptions[i].instanceid === this.instanceid
+			) {
+				description = descriptions[i];
+				break;
+ 			}
+		}
+		if(description) {
+			for(thing in description) {
+				if(description.hasOwnProperty(thing)) {
+					this[thing] = description[thing];
+				}
+			}
+		}
+	}
+
+	this.is_currency = !!this.is_currency;
+	this.tradable = !!this.tradable;
+	this.marketable = !!this.marketable;
+	this.commodity = !!this.commodity;
+	this.market_tradable_restriction = (this.market_tradable_restriction ? parseInt(this.market_tradable_restriction, 10) : 0);
+	this.market_marketable_restriction = (this.market_marketable_restriction ? parseInt(this.market_marketable_restriction, 10) : 0);
+	this.fraudwarnings = this.fraudwarnings || [];
+	this.descriptions = this.descriptions || [];
+
+	if(this.owner && JSON.stringify(this.owner) == '{}') {
+		this.owner = null;
+	}
+}
+
+CEconItem.prototype.getImageURL = function() {
+	return "https://steamcommunity-a.akamaihd.net/economy/image/" + this.icon_url + "/";
+};
+
+CEconItem.prototype.getLargeImageURL = function() {
+	if(!this.icon_url_large) {
+		return this.getImageURL();
+	}
+
+	return "https://steamcommunity-a.akamaihd.net/economy/image/" + this.icon_url_large + "/";
+};
+
+CEconItem.prototype.getTag = function(category) {
+	if(!this.tags) {
+		return null;
+	}
+
+	for(var i = 0; i < this.tags.length; i++) {
+		if(this.tags[i].category == category) {
+			return this.tags[i];
+		}
+	}
+
+	return null;
+};
---
+++ index.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let request = require('request-promise-native');
+//CEconItem from 'McKay's steam packages
+let CEconItem = require('./CEconItem.js');
+
+module.exports = (steamid, appid, contextid, tradableOnly, proxy) => {
+	let inventory = [];
+
+	return request({
+		url: 'http://steamcommunity.com/inventory/'+steamid+'/'+appid+'/'+contextid+'?l=english&count=5000',
+		proxy: proxy || undefined,
+		json: true
+	}).then((res) => {
+		if (res.success && res.total_inventory_count === 0) throw 'No items';
+		if (!res || !res.success || !res.assets || !res.descriptions) throw 'Malformed response';
+
+		for (let item in res.assets) {
+			  if(!res.assets.hasOwnProperty(item)) continue;
+			  const generatedItem = new CEconItem(res.assets[item], res.descriptions, contextid);
+			  if (generatedItem.tradable === tradableOnly) inventory.push(generatedItem);
+		}
+
+		if (res.total_inventory_count < 5000) {
+			return inventory;
+		} else if (res.total_inventory_count >= 5000 && res.last_assetid){
+			let requestChain = Promise.resolve(res.last_assetid);
+
+			for (let i = 0; i < Math.ceil(res.total_inventory_count / 5000) - 1; i++) {
+				requestChain = requestChain.then((lastAssetId) => {
+					return request({
+						url: 'http://steamcommunity.com/inventory/'+steamid+'/'+appid+'/'+contextid+'?l=english&count=5000&start_assetid='+lastAssetId,
+						proxy: proxy || undefined,
+						json: true
+					});
+				}).then((res) => {
+					if (!res || !res.success || !res.assets || !res.descriptions) throw 'Malformed response';
+
+					for (let item in res.assets) {
+						  if(!res.assets.hasOwnProperty(item)) continue;
+						  const generatedItem = new CEconItem(res.assets[item], res.descriptions, contextid);
+						  if (generatedItem.tradable === tradableOnly) inventory.push(generatedItem);
+					}
+
+					return res.last_assetid ? res.last_assetid : inventory;
+				});
+			}
+
+			return requestChain;
+		}
+	})
+};
