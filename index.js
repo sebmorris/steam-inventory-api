0965a9794c7fedf6aad95dcf18492857c6d6f746
Seb Morris <seb.morris@btinternet.com>
2017-09-22 12:20:18 +0100

    Added rotating proxy support

--- index.js
+++ index.js
@@ -1,48 +1,57 @@
 'use strict';
 
-let request = require('request-promise-native');
+const request = require('request-promise-native');
 //CEconItem from 'McKay's steam packages
-let CEconItem = require('./CEconItem.js');
+const CEconItem = require('./CEconItem.js');
+const rotateArray = require('./rotate-array.js');
 
 exports.getInventory = (steamid, appid, contextid, tradableOnly, proxy) => {
-	const makeRequest = (lastAssetId) => {
-		return request({
-			url: 'http://steamcommunity.com/inventory/'+steamid+'/'+appid+'/'+contextid+'?l=english&count=5000'+(lastAssetId ? '&start_assetid='+lastAssetId : ''),
-			proxy: proxy,
-			json: true
-		});
-	};
+  const proxyChoice = () => proxy;
+  if (typeof proxy === 'object') proxyChoice = rotateArray(proxy, 1);
 
-	const parseRes = (res) => {
-		if (res.success && res.total_inventory_count === 0) return inventory;
-		if (!res || !res.success || !res.assets || !res.descriptions) throw 'Malformed response';
+  const makeRequest = (lastAssetId) => {
+    return request({
+      url: 'http://steamcommunity.com/inventory/'+steamid+'/'+appid+'/'+contextid+'?l=english&count=5000'+(lastAssetId ? '&start_assetid='+lastAssetId : ''),
+      proxy: proxyChoice(),
+      json: true
+    });
+  };
 
-		for (let item in res.assets) {
-			  if(!res.assets.hasOwnProperty(item)) continue;
-			  const generatedItem = new CEconItem(res.assets[item], res.descriptions, contextid);
-			  if (!tradableOnly || generatedItem.tradable) inventory.push(generatedItem);
-		}
-	};
+  const parseRes = (res) => {
+    if (res.success && res.total_inventory_count === 0) return inventory;
+    if (!res || !res.success || !res.assets || !res.descriptions) throw 'Malformed response';
 
-	let inventory = [];
+    for (let item in res.assets) {
+      if(!res.assets.hasOwnProperty(item)) continue;
+      const generatedItem = new CEconItem(res.assets[item], res.descriptions, contextid);
+      if (!tradableOnly || generatedItem.tradable) inventory.push(generatedItem);
+    }
+  };
 
-	return makeRequest().then((res) => {
-		if (parseRes(res)) return inventory;
+  // Large inventory hotfix:
+  // - Use proxy ips in chain
+  // Long term
+  // - Simultaneous requests (ie multiple proxy chains)
 
-		if (res.total_inventory_count > 5000 && res.last_assetid) {
-			let requestChain = Promise.resolve(res.last_assetid);
+  let inventory = [];
 
-			for (let i = 0; i < Math.ceil(res.total_inventory_count / 5000) - 1; i++) {
-				requestChain = requestChain.then((lastAssetId) => {
-					return makeRequest(lastAssetId);
-				}).then((res) => {
-					if (parseRes(res)) return inventory;
-					return res.last_assetid ? res.last_assetid : inventory;
-				});
-			}
+  return makeRequest().then((res) => {
+    if (parseRes(res)) return inventory;
 
-			return requestChain;
-		}
-		return inventory;
-	});
+    if (res.total_inventory_count > 5000 && res.last_assetid) {
+      let requestChain = Promise.resolve(res.last_assetid);
+
+      for (let i = 0; i < Math.ceil(res.total_inventory_count / 5000) - 1; i++) {
+        requestChain = requestChain.then((lastAssetId) => {
+          return makeRequest(lastAssetId);
+        }).then((res) => {
+          if (parseRes(res)) return inventory;
+          return res.last_assetid ? res.last_assetid : inventory;
+        });
+      }
+
+      return requestChain;
+    }
+    return inventory;
+  });
 };
